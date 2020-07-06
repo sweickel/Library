a8208917d42a3e19931a4077c73a88173a493570
Sean Weickel <sweickel@gmail.com>
2020-07-05 22:49:55 -0400

    Initial commit  Changes to be committed: 	new file:   index.html 	new file:   scripts/script.js 	new file:   styles/style.css

---
+++ scripts/script.js
@@ -0,0 +1,94 @@
+let lib = [];
+
+
+
+
+//constructor for new book
+function Book(title, author, numPages, read) {
+	this.title = title,
+	this.author = author,
+	this.numPages = numPages,
+	this.read = read
+}
+
+
+
+
+//adds book to lib and displays on page
+function addBook(title, author, pages, read) {
+	
+	//1. take user input and store in lib
+	lib.push(new Book(title, author, pages, read));
+	render();
+}
+
+function render() {
+	
+	//remove all children of parent node wrapper
+	const wrapper = document.querySelector('.lib-wrapper');
+	const wrapperLength = wrapper.children.length;
+	
+	for (let i = wrapperLength - 1; i >=0; i--) {
+		wrapper.removeChild(wrapper.children[i]);
+	}
+	
+
+	//renders each book to DOM
+	lib.forEach((book) => {
+	const item = document.createElement('p');
+	item.classList.add('card-wrapper');
+
+	const bookTitle = document.createElement('h1');
+	bookTitle.classList.add('card-title');
+	bookTitle.textContent = book.title;
+
+	const bookAuthor = document.createElement('h2');
+	bookAuthor.classList.add('card-author');
+	bookAuthor.textContent = book.author;
+
+	const bookPages = document.createElement('h3');
+	bookPages.classList.add('card-pages');
+	bookPages.textContent = `Pages: `+ `${book.numPages}`;
+
+	const bookButton = document.createElement('div')
+	bookButton.classList.add('card-button')
+	const bookRead = document.createElement('p');
+	bookRead.classList.add('card-read-status');
+	
+	if (book.read === true) {
+		bookRead.classList.add('read');
+		bookRead.textContent = "Read";
+	}	else {
+		bookRead.classList.add('unread');	
+	  	bookRead.textContent = "unread";
+	  }
+
+	wrapper.appendChild(item);
+	item.appendChild(bookTitle);
+	item.appendChild(bookAuthor);
+	item.appendChild(bookPages);
+	item.appendChild(bookButton);
+	bookButton.appendChild(bookRead);
+	
+})
+}
+
+
+
+//each book card template
+
+
+
+
+
+
+ex1 = new Book("Astrophysics for People in a Hurry", "Neil DeGrasse Tyson", 208, true);
+ex2 = new Book("The DaVinci Code", "Dan Brown", 597, true);
+ex3 = new Book("Head First JavaScript", "Michael Morrison", 598, false);
+lib.push(ex1,ex2,ex3);
+
+
+
+
+
+
